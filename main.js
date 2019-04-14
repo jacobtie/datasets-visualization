c83ce7858def8635e890dbae2beb5d63f62ca418
jacobtie <jacobtie@gmail.com>
2019-04-14 01:28:05 -0400

    Added style sheet and started layout

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+var datasets;
+
 fetch('datasets.json')
     .then(response => response.json())
-    .then(json => {console.log("Data is loaded."); var datasets = json})
+    .then(json => {console.log("Data is loaded."); datasets = json})
 
