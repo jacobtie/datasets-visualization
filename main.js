946f2118a0e9c0c2ef9acf9b71d6f27b613ca18e
jacobtie <jacobtie@gmail.com>
2019-04-14 16:01:51 -0400

    Wrote function to get subject frequencies from datasets for tree map

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 var datasets;
+var subset;
 
-fetch('datasets.json')
+fetch("datasets.json")
     .then(response => response.json())
-    .then(json => {console.log("Data is loaded."); datasets = json})
+    .then(json => {console.log("Data is loaded."); subset = datasets = json});
 
+function reduceByFreq(arr) {
+    let acc = [];
+    arr.forEach(v => {
+        let found = false;
+        for (let i = 0; i < acc.length; i++) {
+            if (acc[i][0] === v) {
+                acc[i][1]++;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            acc.push([v, 1]);
+        }
+    });
+
+    return acc;
+}
+
+function getKeywordFreqFromSubset() {
+    let keywordFreq = {"name": "Datasets", "children": []};
+
+}
+
+function getSubjectFreqFromDatasets() {
+    let subjectFreq = {"name": "Datasets", "children": []};
+    let allSubjects = datasets.flatMap(d => d["subject"]);
+    reduceByFreq(allSubjects).forEach(s => {
+        subjectFreq["children"].push({"name":s[0], "value":s[1]});
+    });
+    return subjectFreq;
+}
