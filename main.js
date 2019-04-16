aca3394f51699846eb9be9c9eda51e01e4516ba4
jacobtie <jacobtie@gmail.com>
2019-04-16 09:06:08 -0400

    Moved filtering functions to its own js file

--- main.js
+++ main.js
@@ -6,39 +6,3 @@ var keywords = [];
 fetch("datasets.json")
     .then(response => response.json())
     .then(json => {console.log("Data is loaded."); subset = datasets = json});
-
-function selectSubject(sub) {
-    subject = sub;
-    keywords = [];
-    subset = datasets.filter(d => d["subject"].includes(subject));
-}
-
-function filterByKeywords() {
-    subset = datasets.filter(d => d["subject"].includes(subject));
-    subset = subset.filter(d => {
-        let keep = true;
-        for (k of keywords) {
-            if (!d["keywords"].includes(k)) {
-                keep = false;
-                break;
-            }
-        }
-        return keep;
-    });
-}
-
-function selectKeyword(keyword) {
-    keywords.push(keyword);
-    filterByKeywords();
-}
-
-function unselectKeyword(keyword) {
-    keywords = keywords.filter(k => k !== keyword);
-    filterByKeywords();
-}
-
-function clearSelections() {
-    subject = null;
-    keywords = [];
-    subset = datasets;
-}
