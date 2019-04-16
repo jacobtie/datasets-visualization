603a18cc58367da0feaeb96afb06fb7c8b416f64
jacobtie <jacobtie@gmail.com>
2019-04-16 16:27:55 -0400

    Stopped user for clicking last keyword in bubble chart

--- bubbleChart.js
+++ bubbleChart.js
@@ -2,6 +2,7 @@
 function drawBubbleChart() {
     d3.select('#bubble-diagram svg').selectAll('g').remove();
     let keyword = false;
+    let end = false;
     if (subject === null) {
         console.log("Drawing subject bubbles");
         data = getSubjectFreqFromDatasets();
@@ -12,25 +13,27 @@ function drawBubbleChart() {
             data['children'] = data['children'].sort((a, b) => a['value'] > b['value'] ? -1 : 1).slice(0, 10);
         }
         keyword = true;
+        end = data['children'].length === 1;
+        console.log(end);
     }
     let root = d3.hierarchy(data);
-        let packLayout = d3.pack().padding(10);
-        packLayout.size([500, 500]);
-        root.sum(d => d['value']);
-        toAdd = root.descendants().filter(d => d.depth != 0);
-        packLayout(root);
-        nodes = d3.select('#bubble-diagram svg')
-            .selectAll('circle')
-            .data(toAdd)
-            .enter()
-            .append('g')
-            .attr('style', 'cursor:pointer')
-            .attr('transform', d => 'translate(' + [d.x, d.y] + ')')
-            .on('click', d => keyword ? selectKeyword(d.data.name) : selectSubject(d.data.name));
-        nodes.append('circle')
-            .attr('r', d => d.r)
-            .attr('fill', 'white')
-            .attr('stroke', 'black');
-        nodes.append('text')
-            .text(d => d.data.name);
+    let packLayout = d3.pack().padding(10);
+    packLayout.size([500, 500]);
+    root.sum(d => d['value']);
+    toAdd = root.descendants().filter(d => d.depth != 0);
+    packLayout(root);
+    nodes = d3.select('#bubble-diagram svg')
+        .selectAll('circle')
+        .data(toAdd)
+        .enter()
+        .append('g')
+        .attr('style', 'cursor:pointer')
+        .attr('transform', d => 'translate(' + [d.x, d.y] + ')')
+        .on('click', d => end ? null : keyword ? selectKeyword(d.data.name) : selectSubject(d.data.name));
+    nodes.append('circle')
+        .attr('r', d => d.r)
+        .attr('fill', 'white')
+        .attr('stroke', 'black');
+    nodes.append('text')
+        .text(d => d.data.name);
 }
