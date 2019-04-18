71514f1bc868f39e573e203850865723f49c48d1
jacobtie <jacobtie@gmail.com>
2019-04-18 09:49:37 -0400

    Refactored main with jquery to initialize when page loads

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ var subset;
 var subject = null;
 var keywords = [];
 
+$(document).ready(() => initialize());
+
 // Runs when page loads, fetches json, parses, initializes subset and dataset, and calls drawCharts()
-fetch("datasets.json")
+function initialize() {
+    fetch("datasets.json")
     .then(response => response.json())
     .then(json => {console.log("Data is loaded."); subset = datasets = json})
     .then(() => drawCharts());
+}
+
 
 // Draws the charts
 function drawCharts() {
