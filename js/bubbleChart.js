73c50468fc01fd1bb70a31a3547fe291a8e31a91
jacobtie <jacobtie@gmail.com>
2019-04-20 16:27:14 -0400

    Changed size of bubble chart and bubbles and text

--- js/bubbleChart.js
+++ js/bubbleChart.js
@@ -9,8 +9,8 @@ function drawBubbleChart() {
     } else {
         console.log("Drawing keyword bubbles");
         data = getKeywordFreqFromSubset();
-        if (data['children'].length > 10) {
-            data['children'] = data['children'].sort((a, b) => a['value'] > b['value'] ? -1 : 1).slice(0, 10);
+        if (data['children'].length > 25) {
+            data['children'] = data['children'].sort((a, b) => a['value'] > b['value'] ? -1 : 1).slice(0, 25);
         }
         keyword = true;
         end = data['children'].length === 1;
@@ -28,12 +28,14 @@ function drawBubbleChart() {
         .enter()
         .append('g')
         .attr('style', 'cursor:pointer')
-        .attr('transform', d => 'translate(' + [d.x, d.y] + ')')
+        .attr('transform', d => 'translate(' + [d.x*1.7, d.y*1.7] + ')')
         .on('click', d => end ? null : keyword ? selectKeyword(d.data.name) : selectSubject(d.data.name));
     nodes.append('circle')
-        .attr('r', d => d.r)
+        .attr('r', d => d.r*1.7)
         .attr('fill', 'white')
         .attr('stroke', 'black');
     nodes.append('text')
-        .text(d => d.data.name);
+        .text(d => d.data.name)
+        .style("text-anchor", "middle")
+        .attr("font-size", d => d.r/2.5);
 }
