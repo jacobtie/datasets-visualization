b011e82f3e2b12ee199fb361fa3d479970a9b3b8
jacobtie <jacobtie@gmail.com>
2019-04-29 23:05:18 -0400

    Fixed clear button to clear keywords list

--- js/clearButton.js
+++ js/clearButton.js
@@ -1,4 +1,10 @@
 function drawClearButton() {
     const container = document.getElementById('clear');
-    container.innerHTML = '<a href="#" onclick="clearSelections()">Clear</a>';
+    container.innerHTML = '<a href="#" onclick="clearAll()">Clear</a>';
+}
+
+function clearAll() {
+    const container = document.getElementById('keywords');
+    container.innerHTML = '';
+    clearSelections();
 }
