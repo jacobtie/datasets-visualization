1cf3595dd36c610bc11b0764c5595faf07c927d4
jacobtie <jacobtie@gmail.com>
2019-04-29 20:39:19 -0400

    Finished functionality of navbar

---
+++ js/clearButton.js
@@ -0,0 +1,4 @@
+function drawClearButton() {
+    const container = document.getElementById('clear');
+    container.innerHTML = '<a href="#" onclick="clearSelections()">Clear</a>';
+}
---
+++ js/keywordButtons.js
@@ -0,0 +1,43 @@
+function drawKeyword(keyword) {
+    const container = document.getElementById('keywords');
+    const newDiv = document.createElement('div');
+    newDiv.className = 'keyword';
+    newDiv.id = keyword;
+    const newA = document.createElement('a');
+    newA.href = "#";
+    newA.setAttribute('onclick', `removeKeyword("${keyword}")`);
+    newA.innerHTML = "X";
+    newDiv.appendChild(newA);
+    const newSpan = document.createElement('span');
+    newSpan.innerHTML = keyword;
+    newDiv.appendChild(newSpan);
+    container.appendChild(newDiv);
+}
+
+function removeKeyword(keyword) {
+    const container = document.getElementById('keywords');
+    container.removeChild(document.getElementById(keyword));
+    unselectKeyword(keyword);
+}
+
+function drawSubject(subject) {
+    const container = document.getElementById('keywords');
+    const newDiv = document.createElement('div');
+    newDiv.className = 'subject';
+    newDiv.id = subject;
+    const newA = document.createElement('a');
+    newA.href = "#";
+    newA.setAttribute('onclick', `removeSubject()`);
+    newA.innerHTML = "X";
+    newDiv.appendChild(newA);
+    const newSpan = document.createElement('span');
+    newSpan.innerHTML = subject;
+    newDiv.appendChild(newSpan);
+    container.appendChild(newDiv);
+}
+
+function removeSubject() {
+    const container = document.getElementById('keywords');
+    container.innerHTML = "";
+    clearSelections();
+}
