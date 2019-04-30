48e7516e68e0bfc587f501aea64268dac845cf8f
jacobtie <jacobtie@gmail.com>
2019-04-29 23:44:20 -0400

    This is as good as it is going to get

--- js/searchBar.js
+++ js/searchBar.js
@@ -2,7 +2,7 @@ function drawSearchBar() {
     const container = document.getElementById('search');
     container.innerHTML = '';
     const searchBar = document.createElement('select');
-    searchBar.innerHTML = '<option selected="selected" value="default">Select an option</option>';
+    searchBar.innerHTML = '<option selected="selected" value="default">Select</option>';
     searchBar.setAttribute('onchange','runSearch(this)');
     const type = subject === null ? 'subject' : 'keywords';
     const options = reduceByFreq(subset.flatMap(d => d[type])).sort((a, b) => a[1] < b[1] ? 1 : -1).map(s => s[0]).slice(0,1000);
